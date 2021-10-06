e199c11114cfd6ef61b9e3f21aca8d9b04129583
Josh Zuech <josh.zuech@nextthought.com>
2021-10-06 13:47:49 -0500

    Allow multiline desc

--- src/nti/contenttypes/credit/interfaces.py
+++ src/nti/contenttypes/credit/interfaces.py
@@ -25,6 +25,7 @@ from nti.ntiids.schema import ValidNTIID
 from nti.schema.field import Int
 from nti.schema.field import Number
 from nti.schema.field import Object
+from nti.schema.field import ValidText
 from nti.schema.field import ValidDatetime
 from nti.schema.field import IndexedIterable
 from nti.schema.field import DecodingValidTextLine as ValidTextLine
@@ -127,7 +128,7 @@ class IAwardedCredit(ICreated, ILastModified, IContained):
                           min_length=2,
                           required=True)
 
-    description = ValidTextLine(title=u"Description of the awarded credit", required=False)
+    description = ValidText(title=u"Description of the awarded credit", required=False)
 
     credit_definition = Object(ICreditDefinition,
                                title=u'The credit definition',
